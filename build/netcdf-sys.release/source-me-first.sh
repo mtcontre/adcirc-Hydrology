8540bcf2f12d5db3efcb2b12cb14d22e45d95582
Shintaro Bunya <sbunya@unc.edu>
2021-10-08 08:53:32 -0400

    Update source-me-first.sh to correct hdf5 version

--- build/netcdf-sys.release/source-me-first.sh
+++ build/netcdf-sys.release/source-me-first.sh
@@ -4,7 +4,7 @@
 module purge
 module load intelc/18.0.0
 module load intelfort/18.0.0
-module load hdf5/1.10.4_intel-18.0.0
+module load hdf5/1.10.1_intel-18.0.0
 module load mvapich2/2.3b_intel-18.0.0_ch3_ofed-4.1
 module load zlib/1.2.11_intel-18.0.0
 module load netcdf/netcdf-C4.8.0-F4.5.3_intel-18.0.0
