ab80870417002a0d0312b29efa17cda658781d11
mtcontre <mcontre3@nd.edu>
2023-04-11 15:46:08 -0600

    Adding hydrology (precipitation and source nodes river connection) and variable Manning values as nodal attribute

--- cmake/adcprep.cmake
+++ cmake/adcprep.cmake
@@ -13,6 +13,7 @@ if(BUILD_ADCPREP)
       ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_SOURCE_DIR}/src/rs2.F
       ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_SOURCE_DIR}/src/wind.F
       ${CMAKE_SOURCE_DIR}/prep/presizes.F
       ${CMAKE_SOURCE_DIR}/prep/pre_global.F
